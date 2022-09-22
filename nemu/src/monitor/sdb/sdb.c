6dc3a5dcaea9457647a2384b5b10b091646ce04e
tracer-ics2022 <tracer@njuics.org>
2022-09-23 00:48:14 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:48:14 up  2:25,  9 users,  load average: 0.14, 0.18, 0.19

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -109,9 +109,8 @@ static int cmd_x(char *args) {
 
 	/* 输出地址对应的数据 */
 	for (i=0; i< num; i++) {
-		//printf("%x",(uint32_t)paddr_read(addr ,4));
-		printf("%12x\n",(uint32_t)paddr_read(addr ,4));
-		addr += 1024;
+		//printf("%x",(uint32_t)paddr_read(addr++ ,4));
+		printf("%12x\n",(uint32_t)paddr_read(addr++ ,4));
 	}
 	printf("\n");	
 	return 0;
