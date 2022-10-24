6db17d52e4861d222e42ef8d4b7fe42529bbfdd1
tracer-ics2022 <tracer@njuics.org>
2022-10-24 21:59:51 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:59:51 up 10:56,  3 users,  load average: 0.44, 0.50, 0.61

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,11 +9,23 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+	size_t i;
+	for(i = 0; src[i] != '\0'; i++) {
+		dst[i] = src[i];
+	}
+	dst[i] = '\0';
+	return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+	size_t i;
+	for(i = 0; (i < n) && (src[i] != '\0'); i++) {
+		dst[i] = src[i];
+	}
+	for(; i < n; i++){
+		dst[i] = '\0';
+	}
+	return dst;
 }
 
 char *strcat(char *dst, const char *src) {
@@ -21,11 +33,46 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+	char *ps1 = (char *)s1, *ps2 = (char *)s2;
+	while(*ps1 != '\0' && *ps2 != '\0') {
+		if (*ps1 > *ps2){
+			return 1;
+		}
+		else if (*ps1 < *ps2){
+			return -1;
+		}
+		ps1++, ps2++;
+	}
+	if (*ps1 != '\0') {
+		return 1;
+	}
+	else if (*ps2 != '\0'){
+		return -1;
+	}
+	return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+	char *ps1 = (char *)s1, *ps2 = (char *)s2;
+	while(*ps1 != '\0' && *ps2 != '\0' && n>0) {
+		if (*ps1 > *ps2){
+			return 1;
+		}
+		else if (*ps1 < *ps2){
+			return -1;
+		}
+		ps1++, ps2++, n--;
+	}
+	if (n == 0){
+		return 0;
+	}
+	else if (*ps1 != '\0') {
+		return 1;
+	}
+	else if (*ps2 != '\0'){
+		return -1;
+	}
+	return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
