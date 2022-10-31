1428d86907ad90d3a4b74c9d7635cce8503ee345
tracer-ics2022 <tracer@njuics.org>
2022-10-31 21:23:50 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:23:50 up  1:35,  5 users,  load average: 0.27, 0.16, 0.24

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -97,7 +97,13 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+	size_t i;
+	//这里可能会有错，需要byte的type
+	char *src = s;
+	for (i = 0; i < n; i++) {
+		src[i] = c;
+	}
+	return 0;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
