2558576c78a0585d794f90974e673b86dfa2cd35
tracer-ics2022 <tracer@njuics.org>
2022-11-01 13:38:24 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:38:24 up 31 min,  5 users,  load average: 0.19, 0.29, 0.26

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,15 +14,59 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	va_list ap;
+	int d;
+	char c;
+	char *s;
+	char *record_out = out;
+	va_start(ap, fmt);
+	while (*fmt) {
+		if (*fmt == '\\') {
+			*out++ = *++fmt;
+			fmt++;
+		}
+		else {
+			if (*fmt == '%') switch (*fmt++) {//每个分支自己管理out的变化，统一管理fmt的变化
+				case 's':              /* string */
+					s = va_arg(ap, char *);
+					strcpy(out, s);
+					out += strlen(s);
+					break;
+				case 'd':              /* int */
+					d = va_arg(ap, int);
+					if (d == 0) *out++ = '0';
+					else {
+						if (d < 0) *out++ = '-';
+						while(d != 0) {
+							*out++ = d%10 + '0';
+							d = d/10;
+						}
+					}
+					break;
+				case 'c':             /* char */
+															/* need a cast here since va_arg only
+															*   takes fully promoted types */
+					c = (char) va_arg(ap, int);
+					*out++ = c;
+					break;
+				default :
+					panic("uncompleted\n");
+			}
+			else *out = *fmt++;
+		}
+	}
+	*out = '\0';
+	va_end(ap);
+	return out - record_out;
 }
 
+
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+	panic("Not implemented");
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	panic("Not implemented");
 }
 
 #endif
