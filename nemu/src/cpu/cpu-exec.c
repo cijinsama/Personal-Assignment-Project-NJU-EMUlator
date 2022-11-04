7345e4b067c4b9dd7e25a91c5ba676c0ffbce5a1
tracer-ics2022 <tracer@njuics.org>
2022-11-04 18:51:33 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:51:33 up  3:13, 14 users,  load average: 0.49, 0.46, 0.39

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -105,7 +105,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
 		}
 		else if (s->dnpc > func_table[i].min && s->dnpc < func_table[i].max && i != last_pc_in_which_func) {
 			func_stack--;
-			last_pc_in_which_func = func_stack_container[func_stack];
+			if (func_stack > 0) last_pc_in_which_func = func_stack_container[func_stack-1];
 			if (func_stack < 0){
 				assert(0);
 			}
