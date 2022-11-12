6616eb1b55da9c1438ec23d0eb88e4bd86665252
tracer-ics2022 <tracer@njuics.org>
2022-11-12 20:44:38 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:44:38 up  5:43,  8 users,  load average: 0.36, 0.38, 0.35

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,12 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
+	if (inl(KBD_ADDR) == AM_KEY_NONE) {
+		kbd->keydown = false;
+		kbd->keycode = 0;
+	}
+
 	kbd->keydown = inl(KBD_ADDR) & KEYDOWN_MASK;
 	kbd->keycode = inl(KBD_ADDR) & (~KEYDOWN_MASK);
+	
 }
