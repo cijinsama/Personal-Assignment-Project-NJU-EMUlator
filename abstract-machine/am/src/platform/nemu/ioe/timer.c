e5d6cc5e9e0d30bfa15361782b8ccdc1b6adc1f0
tracer-ics2022 <tracer@njuics.org>
2022-11-13 21:51:24 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:51:24 up  5:40, 12 users,  load average: 0.22, 0.44, 0.49

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,7 +5,8 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us =((( uint64_t )inl(RTC_ADDR))<<32) + ( uint64_t ) inl(RTC_ADDR + 4);
+//   uptime->us =((( uint64_t )inl(RTC_ADDR))<<32) + ( uint64_t ) inl(RTC_ADDR + 4);
+  uptime->us =((( uint64_t )inl(RTC_ADDR + 4))<<32) + ( uint64_t ) inl(RTC_ADDR);
 	return;
 }
 
