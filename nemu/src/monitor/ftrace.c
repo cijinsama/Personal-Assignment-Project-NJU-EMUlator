6406c49a2f6f8d0910f8f87a098fc01ca4a37eac
tracer-ics2022 <tracer@njuics.org>
2022-11-14 14:01:33 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:01:33 up 2 min, 12 users,  load average: 0.78, 0.87, 0.38

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -59,11 +59,7 @@ int ini_func_table(char *file){
 			if (ELF32_ST_TYPE(symbo.st_info) == STT_FUNC) {
 				strcpy(func_table[func_table_size].name, name);
 				func_table[func_table_size].min = symbo.st_value;
-				if (strcmp(name, "_start") == 0) {
-					func_table[func_table_size].max = symbo.st_value + 0xffffff;
-					printf("%08x,%s\n", func_table[func_table_size].max, name);
-				}
-				else func_table[func_table_size].max = symbo.st_value + symbo.st_size;
+				func_table[func_table_size].max = symbo.st_value + symbo.st_size;
 				func_table_size++;
 			}
 		}
