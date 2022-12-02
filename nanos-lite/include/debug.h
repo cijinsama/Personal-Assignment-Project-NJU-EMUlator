8280a7905ec4983b53b1475cb586e35ecccb1bb1
tracer-ics2022 <tracer@njuics.org>
2022-12-02 21:12:42 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:12:42 up  7:13, 12 users,  load average: 0.36, 0.50, 0.47

--- nanos-lite/include/debug.h
+++ nanos-lite/include/debug.h
@@ -3,9 +3,16 @@
 
 #include <common.h>
 
+/*
+ * #define Log(format, ...) \
+ * printf("\33[1;35m[%s,%d,%s] " format "\33[0m\n", \
+ * __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+*/
+
 #define Log(format, ...) \
-  printf("\33[1;35m[%s,%d,%s] " format "\33[0m\n", \
+  printf("[%s,%d,%s] " format "\33[0m\n", \
       __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+
 
 #undef panic
 #define panic(format, ...) \
