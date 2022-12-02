55202d818912bcda019c314419e29ef6935c1a45
tracer-ics2022 <tracer@njuics.org>
2022-12-02 20:11:46 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:11:46 up  6:12, 12 users,  load average: 0.75, 0.54, 0.45

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -22,7 +22,7 @@ extern void __am_asm_trap(void);
 
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
-	printf("%p", __am_asm_trap);
+	printf("debug yong de : __am_asm_trap:%08x\n", __am_asm_trap);
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
   // register event handler
