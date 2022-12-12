1226395ff4d2cbf769e19d31998511b7685a8ad1
tracer-ics2022 <tracer@njuics.org>
2022-12-12 15:02:53 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:02:53 up 16:35, 18 users,  load average: 0.59, 0.73, 0.57

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -63,7 +63,7 @@ void naive_uload(PCB *pcb, const char *filename) {
 
 void ReadElfHeader(Elf_Ehdr *elf_header){
 	ReadFile(0, elf_header, sizeof(Elf_Ehdr), 1);
-	Log("GOT e_ident = %p, expected 0x464c457f", elf_header->e_ident);
+	Log("GOT e_ident = %x, expected 0x464c457f", *(uint32_t *)elf_header->e_ident);
 	assert(*(uint32_t *)elf_header->e_ident == 0x464c457f);
 	return;
 }
