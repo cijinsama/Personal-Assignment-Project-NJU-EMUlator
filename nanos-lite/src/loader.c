5c19be1b9b559646cb79aceabee8d5915d181a7d
tracer-ics2022 <tracer@njuics.org>
2022-12-12 15:43:57 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:43:57 up 17:16, 18 users,  load average: 0.71, 0.43, 0.33

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -39,7 +39,7 @@ uintptr_t ini_loader(){
 	//load ramdisk to v mem and set 0
 	Log("header p num:%d", elf_header.e_phnum);
 	Log("header p entsize:%d", elf_header.e_phentsize);
-	for (int i = 0; i < elf_header.e_phnum; i++, program_header_off += elf_header.e_phentsize){
+	for (int i = 0; i < elf_header.e_phnum; i++, program_header_off += sizeof(program_header)){
 		ReadFile(program_header_off, &program_header, sizeof(program_header), 1);
 		if (program_header.p_type == PT_LOAD){
 			ramdisk2vmem(program_header.p_offset, program_header.p_vaddr, program_header.p_memsz);
