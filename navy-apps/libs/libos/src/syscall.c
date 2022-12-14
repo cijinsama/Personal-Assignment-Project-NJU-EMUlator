08ff209a2f678a53c72f5e4387c16c508f54f711
tracer-ics2022 <tracer@njuics.org>
2022-12-14 16:25:20 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:25:20 up 22:59, 22 users,  load average: 0.63, 0.50, 0.41

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -70,7 +70,7 @@ extern char end;
 static intptr_t program_break = 0;
 
 void *_sbrk(intptr_t increment) {
-	if (program_break == 0) program_break = end;
+	if (program_break == 0) program_break = (intptr_t) &end;
 	void *ret = (void *)program_break;
 // 	if (_syscall_(SYS_brk, program_break + increment, 0, 0) == 0){
 	if (_syscall_(SYS_brk, program_break + increment, program_break, increment) == 0){
