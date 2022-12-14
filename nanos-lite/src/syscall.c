5b76ef8405ca971290d8d362508d819386ccd47d
tracer-ics2022 <tracer@njuics.org>
2022-12-14 09:19:30 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:19:30 up 15:53, 19 users,  load average: 0.40, 0.41, 0.49

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,7 +3,10 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+#ifdef CONFIG_STRACE
+	log_write("[STRACE]: system call number: %x", a[0]);
+	Log("[STRACE]: system call number: %x", a[0]);
+#endif
   switch (a[0]) {
 		case SYS_yield: yield(); c->GPRx = 0; return;
 		case SYS_exit: halt(0); return;
