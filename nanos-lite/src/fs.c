2ff579a2f9f5cf46e72201bc6a92a7ce96b55342
tracer-ics2022 <tracer@njuics.org>
2022-12-16 14:04:27 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:04:27 up 14:34, 23 users,  load average: 0.29, 0.45, 0.43

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -87,7 +87,7 @@ size_t do_sys_lseek(int fd, size_t offset, int whence) {
 			file_table[fd].open_offset += offset;
 			break;
 		case SEEK_END:
-			file_table[fd].open_offset = file_table[fd].size + offset;
+			file_table[fd].open_offset = file_table[fd].disk_offset + offset;
 			break;
 	}
   return file_table[fd].open_offset - file_table[fd].disk_offset;
