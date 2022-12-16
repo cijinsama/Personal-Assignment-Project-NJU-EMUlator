844bfcd75166558cf407b0be0b937bb841101eb7
tracer-ics2022 <tracer@njuics.org>
2022-12-16 17:41:48 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:41:48 up 18:11, 23 users,  load average: 0.43, 0.63, 0.57

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -73,7 +73,7 @@ int do_sys_open(const char *path, int flags, int mode) {
 
 size_t do_sys_read(int fd, void *buf, size_t count) {
 	file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, count);
-	file_table[fd].open_offset += count;
+	if (fd >= FD_FB) file_table[fd].open_offset += count;
   return count;
 }
 
@@ -106,7 +106,7 @@ size_t do_sys_write(int fd, const void *buf, size_t count){
 // 	Log("get write file : %d: %xB at %x", fd,count,file_table[fd].open_offset);
 	size_t ret = count;
 	ret = file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, count);
-	file_table[fd].open_offset += count;
+	if (fd >= FD_FB) file_table[fd].open_offset += count;
 	return ret;
 }
 
