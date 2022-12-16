a94e9099136c088ed54a11c776b843c75cf2f9be
tracer-ics2022 <tracer@njuics.org>
2022-12-16 17:33:06 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:33:06 up 18:02, 24 users,  load average: 0.59, 0.64, 0.49

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -3,7 +3,8 @@
 
 int main() {
   NDL_Init(0);
-  while (1) {
+	int i = 0;
+  while (i++ < 5) {
     char buf[64];
     if (NDL_PollEvent(buf, sizeof(buf))) {
       printf("receive event: %s\n", buf);
