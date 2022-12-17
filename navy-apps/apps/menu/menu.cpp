1173200c44b02ea4071684c2a321e6d253874f48
tracer-ics2022 <tracer@njuics.org>
2022-12-17 14:32:15 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:32:15 up 1 day, 15:02, 24 users,  load average: 1.04, 0.84, 0.56

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -141,7 +141,9 @@ static void draw_text_row(char *s, int r) {
 static void display_menu(int n) {
   clear_display();
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
+	printf("debug 1\n");
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
+	printf("debug 2\n");
   printf("Available applications:\n");
   char buf[80];
   int i;
