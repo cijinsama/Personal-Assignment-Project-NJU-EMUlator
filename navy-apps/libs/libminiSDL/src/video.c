6b1c01e8906da4264fe768245ad43dbf40ca0607
tracer-ics2022 <tracer@njuics.org>
2022-12-17 11:35:19 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:35:19 up 1 day, 12:05, 24 users,  load average: 0.71, 0.76, 0.79

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -30,7 +30,9 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 	uint32_t size_per_pixel = src->format->BitsPerPixel / 8;
 
 	//copy
-	for (int i = 0; i < srcrect->h; i++) memcpy(dst->pixels + (offset_dst + i * dst->w) * size_per_pixel, src->pixels + (offset_src + i * src->w)* size_per_pixel, srcrect->w * size_per_pixel);
+	for (int i = 0; i < srcrect->h; i++) 
+		printf("debug : %d\n", i);
+		memcpy(dst->pixels + (offset_dst + i * dst->w) * size_per_pixel, src->pixels + (offset_src + i * src->w)* size_per_pixel, srcrect->w * size_per_pixel);
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
