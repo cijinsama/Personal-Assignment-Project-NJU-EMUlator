e575baf06f7957f62ba1dddfdd45dcc480c5c1a2
tracer-ics2022 <tracer@njuics.org>
2022-12-17 10:41:38 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:41:38 up 1 day, 11:11, 24 users,  load average: 0.78, 0.78, 0.80

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,7 @@ void init_proc() {
 
   // load program here
 
-	naive_uload(current, "/bin/nslider");
+	naive_uload(current, "/bin/menu");
 }
 
 Context* schedule(Context *prev) {
--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -6,11 +6,12 @@
 #include <stdint.h>
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
+	uint32_t size_per_pixel = s->format->BitsPerPixel / 8;
   assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
 	int32_t offset_dst = dstrect->y * src->w + dstrect->x;
 	int32_t offset_src = srcrect->y * src->w + srcrect->x;
-	for (int i = 0; i < srcrect->h; i++) memcpy(dst->pixels + offset_dst, src->pixels + offset_src, src->w * src->h * 4);
+	for (int i = 0; i < srcrect->h; i++) memcpy(dst->pixels + offset_dst, src->pixels + offset_src, src->w * src->h * size_per_pixel);
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
