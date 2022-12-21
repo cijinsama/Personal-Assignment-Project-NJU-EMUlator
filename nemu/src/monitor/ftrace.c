bc4947484b56371e1e94117039a1e40623bacddf
tracer-ics2022 <tracer@njuics.org>
2022-12-21 19:40:28 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:40:28 up  9:40, 26 users,  load average: 0.93, 0.60, 0.57

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -15,7 +15,7 @@ typedef struct {
 } Finfo;
 
 static Finfo file_table[] __attribute__((used)) = {
-// #include "files.h"
+#include "files.h"
 };
 
 
@@ -24,7 +24,7 @@ void ReadElfHeader(FILE *fp, Elf32_Ehdr *elfheader);
 void ReadSectionName(FILE *fp, Elf32_Shdr shstrtab_sect_header, Elf32_Shdr section, char* name);
 void ReadString(FILE *fp, Elf32_Shdr shstrtab_sect_header, Elf32_Off string_offset, char* dst);
 #ifdef CONFIG_FTRACE
-func_add func_table[512];
+func_add func_table[1024];
 #else
 func_add func_table[0];
 #endif
