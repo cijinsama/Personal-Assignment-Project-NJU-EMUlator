05fdfa208d401d88264f221343ffd5e2cecfc870
tracer-ics2022 <tracer@njuics.org>
2022-12-21 11:16:48 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:16:48 up  1:17, 23 users,  load average: 0.95, 0.68, 0.59

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -13,19 +13,15 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-	printf("debug : 1\n");
   FILE *img = fopen(filename, "r");
   fseek(img, 0, SEEK_END);
   int size = ftell(img);
-	printf("debug  4\n");
   char *content = calloc(1, size);
   fseek(img, 0, SEEK_SET);
   fread(content, size, 1, img);
-	printf("debug : 2\n");
   SDL_Surface *img_surface = STBIMG_LoadFromMemory(content, size);
   free(content);
   fclose(img);
-	printf("debug : 3\n");
   return img_surface;
 }
 
