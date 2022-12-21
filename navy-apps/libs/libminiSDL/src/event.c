828a1f4cb4c05b63c236e1b323c340f53270f1f6
tracer-ics2022 <tracer@njuics.org>
2022-12-21 14:24:20 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:24:20 up  4:24, 25 users,  load average: 1.27, 0.74, 0.75

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -2,9 +2,11 @@
 #include <SDL.h>
 #include <string.h>
 #include <stdio.h>
+#include <assert.h>
 
 #define keyname(k) #k,
 
+int SDL_SetKeyState(SDL_Event *ev);
 static const char *keyname[] = {
   "NONE",
   _KEYS(keyname)
@@ -16,13 +18,14 @@ int SDL_PushEvent(SDL_Event *ev) {
 
 
 
+static uint8_t keyboard_status[256];
 #define buffer_for_key_size 16
 static char buffer_for_key[buffer_for_key_size];
 int SDL_PollEvent(SDL_Event *ev) {
 	if (!NDL_PollEvent(buffer_for_key, buffer_for_key_size)) return 0;
 	switch (buffer_for_key[1]){
-		case 'd': ev->type = SDL_KEYDOWN;ev->key.type = SDL_KEYDOWN; break;
-		case 'u': ev->type = SDL_KEYUP;ev->key.type = SDL_KEYUP; break;
+		case 'd': ev->type = SDL_KEYDOWN; ev->key.type = SDL_KEYDOWN; break;
+		case 'u': ev->type = SDL_KEYUP; ev->key.type = SDL_KEYUP; break;
 	}
 	int temp = 0;
 	for (temp = 0; buffer_for_key[temp] != '\n'; temp++){}
@@ -35,17 +38,34 @@ int SDL_PollEvent(SDL_Event *ev) {
 			break;
 		}
 	}
-  return 1;
+	//如果更新按键状态，如果没有更新就返回0
+  return SDL_SetKeyState(ev);
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
+	assert(0);
   return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
+	assert(0);
   return 0;
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  return NULL;
+	//不确定这个实现对不对
+	printf("Unkown right 查一下web\n");
+	assert(0);
+  if (numkeys){
+		return keyboard_status;
+	}
+	assert(0);
+	return NULL;
+}
+int SDL_SetKeyState(SDL_Event *ev) {
+	if(keyboard_status[ev->key.keysym.sym] == ev->key.type) return 0;
+	else{
+		keyboard_status[ev->key.keysym.sym] == ev->key.type;
+		return 1;
+	}
 }
