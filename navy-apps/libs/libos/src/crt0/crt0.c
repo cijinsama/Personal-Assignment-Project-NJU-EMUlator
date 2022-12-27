79794416ac2ac7e35135d5434b85346557b469d1
tracer-ics2022 <tracer@njuics.org>
2022-12-27 16:38:13 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:38:13 up 23:27, 20 users,  load average: 0.58, 0.47, 0.44

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -9,8 +9,8 @@ int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
 	int argc = *args;
-	char **argv = (char **)(args+sizeof(int));
-	char **envp = (char **)(args+sizeof(int) + sizeof(char **));
+	char **argv = *(char ***)(args+sizeof(int));
+	char **envp = *(char ***)(args+sizeof(int) + sizeof(char **));
 	environ = envp;
 
 	printf("&argc : %p\n", args);
