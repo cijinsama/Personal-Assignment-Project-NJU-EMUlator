89c4719d4401702d4f7a1d07eac8a844c6cc5296
tracer-ics2022 <tracer@njuics.org>
2022-12-28 11:45:47 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:45:47 up  1:42, 24 users,  load average: 0.62, 0.72, 0.72

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -5,7 +5,7 @@ static void *pf = NULL;
 void* new_page(size_t nr_page) {
 	void *temp = pf;
 	pf += nr_page * PGSIZE;//每次增加一个页的大小4kB
-	printf("pf = %p", pf);
+	printf("ret = %p", temp);
   return temp;
 }
 
