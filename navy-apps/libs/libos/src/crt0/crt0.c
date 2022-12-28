ec1688e1a09c335363fd334d42cc6ccb9e4fb701
tracer-ics2022 <tracer@njuics.org>
2022-12-28 10:22:14 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:22:14 up 18 min, 23 users,  load average: 0.60, 0.59, 0.61

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -14,19 +14,19 @@ void call_main(uintptr_t *args) {
 	char **envp = *(char ***)(ptr+ (uintptr_t)sizeof(int) +(uintptr_t) sizeof(char **));
 	environ = envp;
 
-	printf("&argc : %p\n", args);
-	printf("argc = %d\n", argc);
-	printf("&argv = %p\n", (char **)(ptr+(uintptr_t) sizeof(int)));
-	printf("argv = %p\n", argv);
-	printf("argv[0] = %p\n", argv[0]);
-	printf("*argv[0] = %s\n", argv[0]);
-	for(int i = 0; i < 5; i++){
-		printf("str %d = %c\n", i,argv[0][i]);
-	}
-	printf("&envp = %p\n", (char **)(ptr+ (uintptr_t)sizeof(int) +(uintptr_t) sizeof(char **)));
-	printf("envp = %p\n", envp);
-	printf("envp[0] = %p\n", envp[0]);
-	printf("*envp[0] = %s\n", envp[0]);
+// 	printf("&argc : %p\n", args);
+// 	printf("argc = %d\n", argc);
+// 	printf("&argv = %p\n", (char **)(ptr+(uintptr_t) sizeof(int)));
+// 	printf("argv = %p\n", argv);
+// 	printf("argv[0] = %p\n", argv[0]);
+// 	printf("*argv[0] = %s\n", argv[0]);
+// 	for(int i = 0; i < 5; i++){
+// 		printf("str %d = %c\n", i,argv[0][i]);
+// 	}
+// 	printf("&envp = %p\n", (char **)(ptr+ (uintptr_t)sizeof(int) +(uintptr_t) sizeof(char **)));
+// 	printf("envp = %p\n", envp);
+// 	printf("envp[0] = %p\n", envp[0]);
+// 	printf("*envp[0] = %s\n", envp[0]);
 
   exit(main(argc, argv, envp));
   assert(0);
