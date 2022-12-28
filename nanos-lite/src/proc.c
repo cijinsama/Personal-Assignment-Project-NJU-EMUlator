2539577e34f3a47f79138a0e13fb4956f49442db
tracer-ics2022 <tracer@njuics.org>
2022-12-28 10:37:53 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:37:53 up 34 min, 23 users,  load average: 0.57, 0.71, 0.70

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -49,8 +49,8 @@ void context_uload(PCB *pcb, char filename[],char *argv[],char *envp[]){
 
 
 	//假设main上面的argc从这个开始
-	uintptr_t main_ebp = (uintptr_t)area.end - 256;
-	char** environ = (char**) (main_ebp + 64);
+	uintptr_t main_ebp = (uintptr_t)area.end - 512;
+	char** environ = (char**) (main_ebp + 128);
 
 	//搜索argcenv的大小，并且获得储存完过后的地址
 	char* current_addr = area.end - 4;
