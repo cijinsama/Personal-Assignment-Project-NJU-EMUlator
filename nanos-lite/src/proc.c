eb3fabf82757e13cc4cdb7a727975dee20490647
tracer-ics2022 <tracer@njuics.org>
2022-12-28 14:38:08 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:38:08 up  4:34, 23 users,  load average: 0.72, 0.63, 0.69

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -185,9 +185,9 @@ size_t execve(const char * filename, char *const argv[], char *const envp[]){
 void init_proc() {
 // 	context_uload(&pcb[0], "/bin/pal", NULL, NULL);
 	context_kload(&pcb[0], hello_fun, "cijin");
-  char *argv1[] = {prog_pal, "--skip", NULL};
+  char *argv1[] = {prog_nterm,  NULL};
   char *envp1[] = {NULL};
-	context_uload(&pcb[1], prog_pal, argv1, envp1);
+	context_uload(&pcb[1], prog_nterm, argv1, envp1);
 	
   switch_boot_pcb();
 
