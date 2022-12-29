ad6ac80de9b94c5b37da9cac38769099b3d7c9ef
tracer-ics2022 <tracer@njuics.org>
2022-12-29 21:22:45 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:22:45 up 1 day, 11:19, 26 users,  load average: 0.53, 0.58, 0.58

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -37,15 +37,15 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
 	if(isa_mmu_check(vaddr, len, type) == MMU_TRANSLATE){
 		Log("translate vaddr");
 		uint32_t base = csr.satp.decode.base << 12;
-// 		Log("base = %08x",(uint32_t) base);
+		Log("base = %08x",(uint32_t) base);
 		uint32_t pd_item = base + (get_PAGE_DIRECTORY(vaddr) << 2);
 		uint32_t pt_addr = paddr_read(pd_item, 4);
 		uint32_t pt_item = (pt_addr >> 12 << 12) | (get_PAGE_TABLE(vaddr) << 2);
-// 		Log("pd_item = %08x", (uint32_t)pd_item);
-// 		Log("pt_addr = %08x", (uint32_t)pt_addr);
-// 		Log("pt_item = %08x", (uint32_t)pt_item);
+		Log("pd_item = %08x", (uint32_t)pd_item);
+		Log("pt_addr = %08x", (uint32_t)pt_addr);
+		Log("pt_item = %08x", (uint32_t)pt_item);
 		uint32_t pg_addr = paddr_read(pt_item, 4);
-// 		Log("pg_addr = %08x", (uint32_t)pg_addr);
+		Log("pg_addr = %08x", (uint32_t)pg_addr);
 		Assert(((pg_addr>>12 << 12) | get_PAGE_INSIDE(vaddr)) == vaddr, "vaddr = %08x, paddr = %08x", (uint32_t)((pg_addr>>12 << 12) | get_PAGE_INSIDE(vaddr)), vaddr);
 		if(type == 0){
 			paddr_write(pt_addr, 4, pt_addr | PTE_A);
