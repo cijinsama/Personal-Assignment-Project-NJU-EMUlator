0de858caec231046c543eafc5299bc7bc7e2c531
tracer-ics2022 <tracer@njuics.org>
2022-12-29 18:50:58 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:50:58 up 1 day,  8:47, 26 users,  load average: 0.60, 0.45, 0.34

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -85,25 +85,25 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 	uintptr_t pd_bias = get_PAGE_DIRECTORY((uintptr_t)va);
 	uintptr_t pt_bias = get_PAGE_TABLE((uintptr_t)va);
 	uint32_t pd_item = *(uint32_t *)((uintptr_t)as->ptr + (pd_bias << 2));//由于每个表项大小为4B
-// 	printf("pdbias : %08x;ptbias : %08x\n", pd_bias, pt_bias);
-// 	printf("ptr = %08x\n", as->ptr);
-// 	printf("pd_item = %08x\n", pd_item);
-// 	printf("*pd_item = %08x\n", (*(uint32_t *)pd_item));
+	printf("pdbias : %08x;ptbias : %08x\n", pd_bias, pt_bias);
+	printf("ptr = %08x\n", as->ptr);
+	printf("pd_item = %08x\n", pd_item);
+	printf("*pd_item = %08x\n", (*(uint32_t *)pd_item));
 	if(!(pd_item & PTE_V)){
     uintptr_t new_page_table = (uintptr_t)pgalloc_usr(PGSIZE);//每个table有1024项，每项4B
-// 		printf("new page = %08x\n", new_page_table);
+		printf("new page = %08x\n", new_page_table);
     pd_item = (pd_item & 0x3ff) | (0xfffffc00u & (new_page_table));//把新开的page地址放到对应的PD里面
     pd_item = (pd_item | PTE_V);
 		*(uint32_t *)((uintptr_t)as->ptr + (pd_bias << 2)) = pd_item;
 	}
-// 	printf("*pd_item = %08x\n", (*(uint32_t *)pd_item));
+	printf("*pd_item = %08x\n", (*(uint32_t *)pd_item));
 	uintptr_t pt_item = *(uint32_t *)((pd_item >> 10 << 12) | (pt_bias << 2));
-//   pt_item = 0xfffffc00u & ((uintptr_t)pa & ~0xfff);
+  pt_item = 0xfffffc00u & ((uintptr_t)pa & ~0xfff);
   pt_item = ((uintptr_t)pa) & 0xfffffc00u;
 	pt_item = pt_item & (PTE_V | PTE_X | PTE_W | PTE_R);
 	*(uint32_t *)((pd_item >> 10 << 12) + (pt_bias << 2)) = pt_item;
 
-// 	printf("pd : %08x;pt : %08x\n", pd_item, pt_item);
+	printf("pd : %08x;pt : %08x\n", pd_item, pt_item);
 }
 
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
