762d52eafab87d1bdc9bdde03c274e497cd70a84
tracer-ics2022 <tracer@njuics.org>
2022-12-29 21:00:51 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:00:51 up 1 day, 10:57, 26 users,  load average: 0.93, 0.61, 0.51

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -34,7 +34,6 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
   for (i = 0; i < LENGTH(segments); i ++) {
     void *va = segments[i].start;
     for (; va < segments[i].end; va += PGSIZE) {
-// 			printf("\t%x/%x\n", va, segments[i].end);
       map(&kas, va, va, 0);
     }
   }
@@ -100,7 +99,9 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   pt_item = ((uintptr_t)pa) & 0xfffffc00u;
 	pt_item = pt_item | (PTE_V | PTE_X | PTE_W | PTE_R);
 	*(uint32_t *)((pd_item >> 12 << 12) + (pt_bias << 2)) = pt_item;
-
+	if( ((pt_item >> 12 << 12) | get_PAGE_INSIDE((uint32_t)va)) != (uint32_t)pa ){
+		printf("va : %08x, pa : %08x\n",((pt_item >> 12 << 12) | get_PAGE_INSIDE((uint32_t)va)), pa);
+	}
 // 	printf("pd : %08x;pt : %08x\n", pd_item, pt_item);
 }
 
