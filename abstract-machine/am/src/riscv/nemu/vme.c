7ccde35cb1f4964b4fe24e0577d5f5ca898ccd51
tracer-ics2022 <tracer@njuics.org>
2022-12-29 22:34:12 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:34:12 up 1 day, 12:30, 26 users,  load average: 0.65, 0.53, 0.47

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -78,33 +78,23 @@ static inline uintptr_t get_PAGE_INSIDE(uintptr_t addr){
 }
 
 void map(AddrSpace *as, void *va, void *pa, int prot) {
-// 	printf("pd : %08x;pt : %08x\n", va, pa);
 	uintptr_t pd_bias = get_PAGE_DIRECTORY((uintptr_t)va);
 	uintptr_t pt_bias = get_PAGE_TABLE((uintptr_t)va);
 	uint32_t pd_item = *(uint32_t *)((uintptr_t)as->ptr + (pd_bias << 2));//由于每个表项大小为4B
-// 	printf("pdbias : %08x;ptbias : %08x\n", pd_bias, pt_bias);
-// 	printf("ptr = %08x\n", as->ptr);
-// 	printf("pd_item = %08x\n", pd_item);
-// 	printf("*pd_item = %08x\n", (*(uint32_t *)pd_item));
 	if(!(pd_item & PTE_V)){
     uintptr_t new_page_table = (uintptr_t)pgalloc_usr(PGSIZE);//每个table有1024项，每项4B
-// 		printf("new page = %08x\n", new_page_table);
     pd_item = (pd_item & 0x3ff) | (0xfffffc00u & (new_page_table >> 2));//把新开的page地址放到对应的PD里面
     pd_item = (pd_item | PTE_V);
 		*(uint32_t *)((uintptr_t)as->ptr + (pd_bias << 2)) = pd_item;
 	}
-// 	printf("pd_item = %08x\n", (pd_item));
 	uintptr_t pt_item = *(uint32_t *)((pd_item >> 10 << 12) | (pt_bias << 2));
   pt_item = 0xfffffc00u & (((uintptr_t)pa & ~0xfff) >> 2);
-	pt_item = pt_item | (PTE_V | PTE_X | PTE_W | PTE_R);
+	pt_item = pt_item | PTE_V | PTE_X | PTE_W | PTE_R;
 	*(uint32_t *)((pd_item >> 10 << 12) + (pt_bias << 2)) = pt_item;
 	if( (*(uint32_t *)((pd_item >> 10 << 12) + (pt_bias << 2))) >> 10 << 12 != (uint32_t)pa ){
 		printf("va : %08x, pa : %08x\n",(*(uint32_t *)((pd_item >> 10 << 12) + (pt_bias << 2))) >> 10 << 12, pa);
 		panic("va != pa");
 	}
-// 	printf("va : %08x, pa : %08x\n",((pt_item >> 12 << 12) | get_PAGE_INSIDE((uint32_t)va)), pa);
-// 	printf("pd : %08x;pt : %08x\n", pd_item, pt_item);
-// 	assert(0);
 }
 
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
