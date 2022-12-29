100f59fa0e72da471eff45784080b8b5c3feb6e5
tracer-ics2022 <tracer@njuics.org>
2022-12-29 21:40:52 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:40:52 up 1 day, 11:37, 26 users,  load average: 0.49, 0.49, 0.52

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -95,17 +95,18 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 		assert(*(uint32_t *)((uintptr_t)as->ptr + (pd_bias << 2)) == pd_item);
 	}
 // 	printf("pd_item = %08x\n", (pd_item));
-	uintptr_t pt_item = *(uint32_t *)((pd_item >> 12 << 12) | (pt_bias << 2));
+	uintptr_t pt_item = *(uint32_t *)((pd_item >> 10 << 12) | (pt_bias << 2));
   pt_item = 0xfffffc00u & ((uintptr_t)pa & ~0xfff);
   pt_item = ((uintptr_t)pa) & 0xfffffc00u;
 	pt_item = pt_item | (PTE_V | PTE_X | PTE_W | PTE_R);
-	*(uint32_t *)((pd_item >> 12 << 12) + (pt_bias << 2)) = pt_item;
-// 	if( ((pt_item >> 12 << 12) | get_PAGE_INSIDE((uint32_t)va)) != (uint32_t)pa ){
-// 		printf("va : %08x, pa : %08x\n",((pt_item >> 12 << 12) | get_PAGE_INSIDE((uint32_t)va)), pa);
-// 	}
-	printf("va : %08x, pa : %08x\n",((pt_item >> 12 << 12) | get_PAGE_INSIDE((uint32_t)va)), pa);
-	printf("pd : %08x;pt : %08x\n", pd_item, pt_item);
-	assert(0);
+	*(uint32_t *)((pd_item >> 10 << 12) + (pt_bias << 2)) = pt_item;
+	if( ((pt_item >> 10 << 12) | get_PAGE_INSIDE((uint32_t)va)) != (uint32_t)pa ){
+		printf("va : %08x, pa : %08x\n",((pt_item >> 10 << 12) | get_PAGE_INSIDE((uint32_t)va)), pa);
+		panic("va != pa");
+	}
+// 	printf("va : %08x, pa : %08x\n",((pt_item >> 12 << 12) | get_PAGE_INSIDE((uint32_t)va)), pa);
+// 	printf("pd : %08x;pt : %08x\n", pd_item, pt_item);
+// 	assert(0);
 }
 
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
