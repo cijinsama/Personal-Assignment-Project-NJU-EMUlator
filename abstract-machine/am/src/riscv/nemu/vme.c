e757d67eae0a23555f0c3a96d7c61a2daa10f6f0
tracer-ics2022 <tracer@njuics.org>
2022-12-30 20:01:51 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:01:51 up 2 days,  9:58, 24 users,  load average: 0.37, 0.31, 0.25

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -107,7 +107,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
 	Context *context = kstack.end - sizeof(Context);
 	context->mepc = (uintptr_t)	entry;
-	context->mstatus = 0x1880;
+	context->mstatus = 0x1800;
 	context->pdir = as->ptr;
   return context;
 }
