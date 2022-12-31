aaa2190dcdc10c9040d7dfd04ba97632c3f5933e
tracer-ics2022 <tracer@njuics.org>
2022-12-31 15:11:19 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:11:19 up 3 days,  5:07, 23 users,  load average: 1.08, 0.83, 0.72

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -31,7 +31,7 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
 #ifndef CONFIG_TARGET_AM
 static int temp = 0;
 static void timer_intr() {
-	if(temp++ < 100){
+	if(temp++ < 5){
 		return;
 	}
 	temp = 0;
