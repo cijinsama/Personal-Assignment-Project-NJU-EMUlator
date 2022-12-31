7fba915eaed84593b00f3d88d6a92e2fb053b3cb
tracer-ics2022 <tracer@njuics.org>
2022-12-31 16:32:35 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:32:35 up 3 days,  6:28, 24 users,  load average: 0.44, 0.54, 0.56

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -2,7 +2,7 @@
 #include <proc.h>
 #include <memory.h>
 
-#define MAX_NR_PROC 6
+#define MAX_NR_PROC 4
 
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
@@ -187,11 +187,11 @@ size_t execve(const char * filename, char *const argv[], char *const envp[]){
 void init_proc() {
   Log("Initializing processes...");
 	context_kload(&pcb[0], hello_fun, "cijin");
-// 	context_kload(&pcb[1], hello_fun, "liuyi");
-  char *argv1[] = {prog_pal,"--skip" ,NULL};
+	context_kload(&pcb[1], hello_fun, "liuyi");
+//   char *argv1[] = {prog_pal,"--skip" ,NULL};
 //   char *argv1[] = {prog_hello ,NULL};
-  char *envp1[] = {NULL};
-	context_uload(&pcb[1], prog_pal, argv1, envp1);
+//   char *envp1[] = {NULL};
+// 	context_uload(&pcb[1], prog_pal, argv1, envp1);
 // 	context_uload(&pcb[1], prog_hello, argv1, envp1);
 	
   switch_boot_pcb();
