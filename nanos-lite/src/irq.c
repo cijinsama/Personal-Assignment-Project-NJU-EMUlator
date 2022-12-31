78716408c6c499bea9eca4f9dc4f81a3887ccbd0
tracer-ics2022 <tracer@njuics.org>
2022-12-31 22:13:18 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:13:18 up 3 days, 12:09, 24 users,  load average: 0.47, 0.56, 0.55

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,12 +5,11 @@ static Context* do_event(Event e, Context* c) {
   switch (e.event) {
 		case EVENT_YIELD:			c = schedule(c);	break;
 		case EVENT_SYSCALL:		do_syscall(c);		break;
-		case EVENT_IRQ_TIMER: c = schedule(c);	break;
+		case EVENT_IRQ_TIMER: Log("timer");c = schedule(c);	break;
     default: panic("Unhandled event ID = %d", e.event);
   }
   return c;
 }
-
 
 void init_irq(void) {
   Log("Initializing interrupt/exception handler...");
