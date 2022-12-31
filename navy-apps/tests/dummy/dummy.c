e3fa012b61a409220e07c9f902a09f29d523d304
tracer-ics2022 <tracer@njuics.org>
2022-12-31 11:53:01 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:53:01 up 3 days,  1:49, 21 users,  load average: 1.10, 0.68, 0.68

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -8,5 +8,11 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
+	int i  = 1;
+	while(1){
+// 		printf("123\n");
+		i++;
+// 		_syscall_(SYS_yield, 0, 0, 0);
+	}
   return _syscall_(SYS_yield, 0, 0, 0);
 }
