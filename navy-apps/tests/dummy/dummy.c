9164ef8d321dc58d59f75de26aeae0ff95c5acf6
tracer-ics2022 <tracer@njuics.org>
2023-01-01 11:41:41 +0800

    >  compile NEMU 201870262 刘一 Linux cijin-MacBookAir 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:41:41 up 4 days,  1:38, 20 users,  load average: 0.56, 0.49, 0.54

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,13 +1,5 @@
 #include <stdint.h>
-#include <string.h>
 #include <stdio.h>
-
-#ifdef __ISA_NATIVE__
-#error can not support ISA=native
-#endif
-
-#define SYS_yield 1
-extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
 	while(1){
